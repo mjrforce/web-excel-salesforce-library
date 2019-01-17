48120a2b550e1ce19273512720d3b4e1f61e6f0e
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-17 10:40:52 -0500

    update

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static('dist'))
+app.use(express.static('dist'));
+app.use(express.static('node_modules'))
 
 app.listen(app.get('port'), function() {
   console.log("Node app running at localhost:" + app.get('port'));

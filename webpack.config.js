d0329c5d921e36300519506c1815aa9fa1dd2da7
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-16 22:04:08 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'polyfill': 'babel-polyfill',
+        'app': './src/index.js',
+        'function-file': './function-file/function-file.js'
+    },
+	output: { publicPath: '/' },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            chunks: ['polyfill', 'app']
+        }),
+        new HtmlWebpackPlugin({
+            template: './function-file/function-file.html',
+            filename: 'function-file/function-file.html',
+            chunks: ['function-file']
+        }),
+		new CopyWebpackPlugin([
+			{ from: './src/assets',
+		      to: '/assets',
+			  toType: 'dir'
+			}
+		]),
+		new CopyWebpackPlugin([
+			{ from: './src/app/app.css' }
+		])
+		
+]};

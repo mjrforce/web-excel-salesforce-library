da09291a1a44a5b82ab25b51a79727d6d199040c
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-17 11:37:36 -0500

    update for oauth

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
+var jsforce = require('jsforce');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('dist'));
 app.use(express.static('node_modules'))
+
+
+var oauth2 = new jsforce.OAuth2({
+  clientId : process.env.CLIENT_ID,
+  clientSecret : process.env.CLIENT_SECRET,
+  redirectUri : process.env.LOGIN_URL
+});
+
+app.get('/oauth2/auth', function(req, res) {
+  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app running at localhost:" + app.get('port'));
--- src/index.js
+++ src/index.js
@@ -5,15 +5,34 @@
 
 import * as OfficeHelpers from '@microsoft/office-js-helpers';
 
-$(document).ready(() => {
-    $('#run').click(run);
-});
-  
+var dialog = null;
 // The initialize function must be run each time a new page is loaded
-Office.initialize = (reason) => {
-    $('#sideload-msg').hide();
-    $('#app-body').show();
-};
+Office.onReady()
+       .then(function() {
+           $(document).ready(function () {  
+		     $('#run').click(run);
+			 $('#sideload-msg').hide();
+             $('#app-body').show();
+			 Office.context.ui.displayDialogAsync(
+			   '/oauth2/auth',
+			   {height: 45, width: 55},
+
+			   function (result) {
+				   dialog = result.value;
+				   dialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogMessageReceived, processMessage);
+			   }
+			);
+               
+
+           });
+       });
+
+   // TODO2: Create the OK button handler
+
+function processMessage(arg) {
+    $('#user-name').text(arg.message);
+    dialog.close();
+}
 
 async function run() {
     try {

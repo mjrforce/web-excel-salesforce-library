5a4092bd4ae807a7193076fedab5b18c6792653e
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-17 13:15:00 -0500

    update

--- src/index.js
+++ src/index.js
@@ -24,25 +24,14 @@ function createTable() {
 	console.log('creating table');
    Excel.run(function (context) {
 
-        var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-		var expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
-		expensesTable.name = "ExpensesTable";
+    var sheet = context.workbook.worksheets.getActiveWorksheet();
+    // Write the data into the range first 
+    var range = sheet.getRange("A1:B3");
+    range.values = [["Key", "Value"], ["A", 1], ["B", 2]];
 
-       expensesTable.getHeaderRowRange().values =
-	   [["Date", "Merchant", "Category", "Amount"]];
-
-	expensesTable.rows.add(null, [
-	   ["1/1/2017", "The Phone Company", "Communications", "120"],
-	   ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
-	   ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
-	   ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
-	   ["1/11/2017", "Bellows College", "Education", "350.1"],
-	   ["1/15/2017", "Trey Research", "Other", "135"],
-	   ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
-	]);
-
-    expensesTable.columns.getItemAt(3).getRange().numberFormat = [['€#,##0.00']];
-
+    // Create the table over the range
+    var table = sheet.tables.add('A1:B3', true);
+    table.name = "Example";
 
        return context.sync();
    })

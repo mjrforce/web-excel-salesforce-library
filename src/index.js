c8f8b2d6389900073ad1983b12a2e694b275b9c9
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-17 12:13:15 -0500

    update

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ var dialog = null;
 $(document).ready(function () {  
 		     $('#run').click(run);
 			 $('#open-dialog').click(openDialog);
+			 $('#create-table').click(createTable);
 });
 		   
 Office.initialize = (reason) => {
@@ -18,6 +19,40 @@ Office.initialize = (reason) => {
     $('#app-body').show();
 	
 };
+
+function createTable() {
+   Excel.run(function (context) {
+
+        var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+		var expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
+		expensesTable.name = "ExpensesTable";
+
+       expensesTable.getHeaderRowRange().values =
+	   [["Date", "Merchant", "Category", "Amount"]];
+
+	expensesTable.rows.add(null /*add at the end*/, [
+	   ["1/1/2017", "The Phone Company", "Communications", "120"],
+	   ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
+	   ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
+	   ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
+	   ["1/11/2017", "Bellows College", "Education", "350.1"],
+	   ["1/15/2017", "Trey Research", "Other", "135"],
+	   ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
+	]);
+
+    expensesTable.columns.getItemAt(3).getRange().numberFormat = [['€#,##0.00']];
+	expensesTable.getRange().format.autofitColumns();
+	expensesTable.getRange().format.autofitRows();
+
+       return context.sync();
+   })
+   .catch(function (error) {
+       console.log("Error: " + error);
+       if (error instanceof OfficeExtension.Error) {
+           console.log("Debug info: " + JSON.stringify(error.debugInfo));
+       }
+   });
+}
 
 function openDialog() {
 			 Office.context.ui.displayDialogAsync(

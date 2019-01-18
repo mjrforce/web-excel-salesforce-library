00172f2d3db4a68270372cf75b97f6ef8a65fed8
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-17 22:24:13 -0500

    update

--- server.js
+++ server.js
@@ -1,43 +1,13 @@
 var express = require('express');
 var app = express();
-var jsforce = require('jsforce');
 var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('dist'));
 app.use(express.static('node_modules'))
 
-var oauth2 = new jsforce.OAuth2({
-  loginUrl: process.env.LOGIN_URL,
-  clientId : process.env.CLIENT_ID,
-  clientSecret : process.env.CLIENT_SECRET,
-  redirectUri : process.env.REDIRECT_URI
-});
-
-app.get('/', function(req, res) {
-  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
-});
-
-app.get('/oauth2/auth', function(req, res) {
-  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
-});
-
 app.get('/oauth2/callback', function(req, res) {
-  var conn = new jsforce.Connection({ oauth2 : oauth2 });
-  var code = req.param('code');
-  conn.authorize(code, function(err, userInfo) {
-    if (err) { return console.error(err); }
-    // Now you can get the access token, refresh token, and instance URL information.
-    // Save them to establish connection next time.
-    console.log(conn.accessToken);
-    console.log(conn.refreshToken);
-    console.log(conn.instanceUrl);
-    console.log("User ID: " + userInfo.id);
-    console.log("Org ID: " + userInfo.organizationId);
-
-    // ...
     res.sendFile(path.join(__dirname + '/dist/callback.html'));
-  });
 });
 
 app.listen(app.get('port'), function() {
--- src/index.js
+++ src/index.js
@@ -2,83 +2,86 @@
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
-
 import * as OfficeHelpers from '@microsoft/office-js-helpers';
-
+var jsforce = require('jsforce');
 var dialog = null;
 // The initialize function must be run each time a new page is loaded
 
-$(document).ready(function () {  
-		     $('#run').click(run);
-			 $('#open-dialog').click(openDialog);
-			 $('#create-table').click(createTable);
+var oauth2 = new jsforce.OAuth2({
+    loginUrl: process.env.LOGIN_URL,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI
+});
+
+$(document).ready(function() {
+    $('#run').click(run);
+    $('#open-dialog').click(openDialog);
+    $('#create-table').click(createTable);
 });
 
 var dlg;
-	   
+
 Office.initialize = (reason) => {
     $('#sideload-msg').hide();
     $('#app-body').show();
-	
 };
 
-function createTable() {
-	console.log('creating table');
-   Excel.run(function (context) {
-
-    var sheet = context.workbook.worksheets.getActiveWorksheet();
-    // Write the data into the range first 
-    var range = sheet.getRange("A1:B3");
-    range.values = [["Key", "Value"], ["A", 1], ["B", 2]];
-
-    // Create the table over the range
-    var table = sheet.tables.add('A1:B3', true);
-    table.name = "Example";
-
-       return context.sync();
-   })
-   .catch(function (error) {
-       console.log("Error: " + error);
-       if (error instanceof OfficeExtension.Error) {
-           console.log("Debug info: " + JSON.stringify(error.debugInfo));
-       }
-   });
-}
-
-function callback(url){
-	$('#result').val(url);
-	
-}
 function openDialog() {
 
-	    var signInUrl = '/oauth2/auth';
-        Office.context.ui.displayDialogAsync(signInUrl,
-            { height: 70, width: 40},
-            function (result) {
-                dlg = result.value;
-                dlg.addEventHandler("dialogMessageReceived", processMessage);
-            });
-    }
-
-    function processMessage(arg) {
-		console.log('event handled');
-	}
-			 
-			 
-			 
+    var signInUrl = oauth2.getAuthorizationUrl({
+        scope: 'api id web'
+    });
+    console.log(signInUrl);
+    Office.context.ui.displayDialogAsync(signInUrl, {
+            height: 70,
+            width: 40
+        },
+        function(result) {
+            dlg = result.value;
+            dlg.addEventHandler("dialogMessageReceived", processMessage);
+        });
+}
 
 function processMessage(arg) {
-    $('#user-name').text(arg.message);
+    console.log(arg.message);
     dialog.close();
+}
+
+function createTable() {
+    console.log('creating table');
+    Excel.run(function(context) {
+
+            var sheet = context.workbook.worksheets.getActiveWorksheet();
+            // Write the data into the range first 
+            var range = sheet.getRange("A1:B3");
+            range.values = [
+                ["Key", "Value"],
+                ["A", 1],
+                ["B", 2]
+            ];
+
+            // Create the table over the range
+            var table = sheet.tables.add('A1:B3', true);
+            table.name = "Example";
+
+            return context.sync();
+        })
+        .catch(function(error) {
+            console.log("Error: " + error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+            }
+        });
 }
 
 async function run() {
     try {
-            await Excel.run(async context => {
-                /**
-                 * Insert your Excel code here
-                 */
-                const range = context.workbook.getSelectedRange();
+        await Excel.run(async context => {
+            /**
+             * Insert your Excel code here
+             */
+            const range = context.workbook.getSelectedRange();
 
             // Read the range address
             range.load('address');
@@ -88,9 +91,9 @@ async function run() {
 
             await context.sync();
             console.log(`The range address was ${range.address}.`);
-            });
-        } catch (error) {
-            OfficeHelpers.UI.notify(error);
-            OfficeHelpers.Utilities.log(error);
-        }
+        });
+    } catch (error) {
+        OfficeHelpers.UI.notify(error);
+        OfficeHelpers.Utilities.log(error);
+    }
 }

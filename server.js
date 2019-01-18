51e03b5a4d04fb99aabca8c4d070326662f5cae8
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-18 14:42:17 -0500

    update

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname + '/dist'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 var settings = {
     loginUrl: process.env.LOGIN_URL,
@@ -19,59 +21,60 @@ var settings = {
 };
 var oauth2 = new jsforce.OAuth2(settings);
 
-
 app.get('/data/query', function(req, res) {
 
-	var data = req.query;
-	var conn = new jsforce.Connection(data.connection);
-	
-	conn.query(data.q, function(err, result) {
-	  if (err) { return console.error(err); }
-	  console.log("total : " + result.totalSize);
-	  console.log("fetched : " + result.records.length);
-	  res.json(result);
-	});
+    var data = req.query;
+    var conn = new jsforce.Connection(data.connection);
 
-	
+    conn.query(data.q, function(err, result) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("total : " + result.totalSize);
+        console.log("fetched : " + result.records.length);
+        res.json(result);
+    });
 });
 
 app.get('/', function(req, res) {
-	var loginUrl = oauth2.getAuthorizationUrl({ scope : 'api id web' });
-	res.render(__dirname + '/dist/index.html', {loginUrl: loginUrl });
-}); 
+    var loginUrl = oauth2.getAuthorizationUrl({
+        scope: 'api id web'
+    });
+    res.render(__dirname + '/dist/index.html', {
+        loginUrl: loginUrl
+    });
+});
 
 app.get('', function(req, res) {
-	res.render(__dirname + '/dist/index.html', settings);
+    res.render(__dirname + '/dist/index.html', settings);
 });
 
 app.get('/oauth2/auth', function(req, res) {
-	res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
+    res.redirect(oauth2.getAuthorizationUrl({
+        scope: 'api id web'
+    }));
 });
 
 app.get('/oauth2/callback', function(req, res) {
-  var conn = new jsforce.Connection({ oauth2 : oauth2 });
-  var code = req.param('code');
-  conn.authorize(code, function(err, userInfo) {
-    if (err) { return console.error(err); }
-    // Now you can get the access token, refresh token, and instance URL information.
-    // Save them to establish connection next time.
-    console.log(conn.accessToken);
-    console.log(conn.refreshToken);
-    console.log(conn.instanceUrl);
-	console.log(JSON.stringify(userInfo));
-	conn.userId = userInfo.id;
-	conn.orgId = userInfo.organizationId;
-    console.log("User ID: " + userInfo.id);
-    console.log("Org ID: " + userInfo.organizationId);
-	res.render(__dirname + '/dist/callback.html', conn);
-  });   
+    var conn = new jsforce.Connection({
+        oauth2: oauth2
+    });
+    var code = req.param('code');
+    conn.authorize(code, function(err, userInfo) {
+        if (err) {
+            return console.error(err);
+        }
+        conn.userId = userInfo.id;
+        conn.orgId = userInfo.organizationId;
+        res.render(__dirname + '/dist/callback.html', conn);
+    });
 });
 
 app.use(express.static('dist'));
 app.use(express.static('node_modules'));
 
 app.listen(app.get('port'), function() {
-  console.log("Node app running at localhost:" + app.get('port'));
+    console.log("Node app running at localhost:" + app.get('port'));
 });
 
 module.exports = app;

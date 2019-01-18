d4be6be403e248737d8ed41a68e620289f3460f3
Matthew Rumschlag <mrumschlag@gmail.com>
2019-01-17 20:32:46 -0500

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var jsforce = require('jsforce');
 var path = require('path');
-var eventManager = require(path.join(__dirname + '/dist/eventManager.js'));
+var eventManager = require('/eventManager.js');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('dist'));
